fcf4d0235eda6e1dfe88b25012c52b326e390ee1
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-04-29 22:12:17 +0600

    array syntax

---
+++ array/array.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+func main(){
+	
+	var fruitList [4]string
+	fruitList[0]="Apple"
+	fruitList[1]="Orange"
+	fruitList[3]="Banana"
+	fmt.Println(fruitList)
+	var fruits=[3]string{"banana","apple","orange"}
+	fmt.Println(fruits) 
+}
